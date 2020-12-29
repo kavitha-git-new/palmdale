246cb72376f900386d96359e83b8f3528a4ab93c
Kavitha Udayakumar <kavitha.jdm@gmail.com>
2020-12-29 17:42:27 +0530

    Afetr making changes in user component

--- src/app/components/pages/update-content/user/user.component.ts
+++ src/app/components/pages/update-content/user/user.component.ts
@@ -178,14 +178,16 @@ export class UserComponent implements OnInit {
             this.succMsg= this.user.fname+" "+this.user.lname+" is saved";
             this.users=[];
             this.getUsers();
-            this.modalService.close("exampleModal")
+            this.modalService.close("exampleModal");
+            return true;
           }
           else{
            alert(JSON.parse(JSON.stringify(response)).response.message);
+           this.errMsg="Please try again"
+           return false;
           }
         });
-        this.modalService.close("exampleModal")
-        return true;
+         return ;
       }
 
 
